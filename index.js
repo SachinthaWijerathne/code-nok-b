f20feb98ba6ba9d21a6b2a5ec832e0b285ff1185
Sachintha <127247647+SachinthaWijerathne@users.noreply.github.com>
2025-07-05 23:10:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 const allowedOrigins = [
   "http://localhost:5173",
+  "http://192.168.1.54",
   "https://nok.web.app",
   "https://admin-nok.web.app",
   "https://nok--test-33qiagfz.web.app"
